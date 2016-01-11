940d77d6a398d70bad5bbc720ec03f6383001b78
Villor 紫刃 <vrbvillor@126.com>
2016-01-11 15:57:15 +0800

    增加了在指定位置处循环功能

--- jq/sprite.js
+++ jq/sprite.js
@@ -1,5 +1,6 @@
 //扩展JQ元素的visible/hidden方法，及sprite
 define(function(require,exports,module){
+
 	$.fn.visible=function(){return this.css('visibility','visible');}
 	$.fn.hidden=function(){return this.css('visibility','hidden');}
 	/*
@@ -19,9 +20,12 @@ define(function(require,exports,module){
 			maxnum，必需，最大数字，超过最大数字就停止填充
 			delta，取图间隔值，默认为1，即每张图都取，可以是正整数，或一个函数
 				*****函数接受两个参数，当前数及最大数，可以使用函数创造衰减的间隔值
-		play/rewind(speed,loop)，正向/反向播放全图，其中参数
+		play(settings)，控制播放，参数为配置集合对象，可以使用的属性有
 			speed，每张图间隔的毫秒数，默认为50
 			loop，布尔值，是否循环播放，默认为0，可以为正整数，为-1时无限循环
+			from，从第几张开始播放，可以为0~n-1，也可以为关键字all（即n-1）
+			to，播放到第几张，说明同from
+		forward/rewind(speed,loop)，正向/反向播放全图，参数同上
 	*/
 	$.fn.sprite=function(setFuns){
 		var THIS=this,
@@ -31,7 +35,9 @@ define(function(require,exports,module){
 				show:$.noop,
 				hide:$.noop,
 				loop:$.noop,
-				stop:$.stop
+				stopped:$.noop,
+				played:$.noop,
+				rewound:$.noop
 			}, setFuns),
 			timer
 		;
@@ -40,30 +46,34 @@ define(function(require,exports,module){
 			step:1,
 			at:0,
 			fill:function(srcPattern,maxnum,delta){
-				if(isNaN(maxnum) || maxnum<=0) return false;
+				if(isNaN(maxnum) || maxnum<=0) {console.error("maxnum不对");return false;}
 				THIS.empty();
 				this.loaded=false;
 				this.frames=0;
 				var SPRITE=this,
 					counter=0,
-					x;
+					x,
+					nums=[];
 				if(isNaN(delta) && !$.isFunction(delta) || delta<1) delta=1;
-				SPRITE.loaded=false;
 				for(var n=1;n<=maxnum;n+=x)
 				{
+					nums.push(n);
+					x=$.isFunction(delta) ? delta(n,maxnum) : delta;
+				}
+				for(var n in nums)
+				{
 					var img=new Image();
-					img.src=srcPattern.replace(/\{n\}/ig,n);
+					img.src=srcPattern.replace(/\{n\}/ig,nums[n]);
 					n-1 && $(img).hidden();
 					$(img).appendTo(THIS);
 					if(img.complete) done();
 					else img.onload=done;
-					x=$.isFunction(delta) ? delta(n,maxnum) : delta;
 				}
 				function done()
 				{
 					counter++;
 					funs.loading(counter,maxnum);
-					if(counter==THIS.find("img").length)
+					if(nums.length==counter)
 					{
 						SPRITE.loaded=true;
 						funs.loaded(counter);
@@ -72,50 +82,61 @@ define(function(require,exports,module){
 				return this;
 			},
 			timer:0,
-			play:function(piSpeed,iLoop){
-				if(!piSpeed || isNaN(piSpeed)) piSpeed=50;
-				if(isNaN(iLoop)) iLoop=0;
+			play:function(settings){
+				var options={
+					speed:50,
+					loop:0,
+					from:0,
+					to:'all',
+					key:false
+				};
+				$.extend(options, settings);
 				var imgs=THIS.find("img"),
 					l=imgs.length,
-					n=0,
+					cur=options.from=='all' ? l-1 : options.from,
+					loops=options.loop,
+					key=loops && (options.key===false ? cur : options.key),
+					end=options.to=='all' ? l-1 : options.to,
 					SPRITE=this,
-					step=this.step;
+					reversed=cur>end,
+					dir=reversed ? -1:1,
+					step=this.step * dir;
 				timer=setInterval(function(){
-					imgs.eq(n).visible();
-					funs.show(n);
-					SPRITE.at=n;
-					var last=(n-step+l)%l;
-					imgs.eq(last).hidden();
+					var last=(cur-step+l)%l;
+					SPRITE.at=cur;
+					imgs.eq(cur).visible().siblings().hidden();
+					funs.show(cur);
 					funs.hide(last);
-					n+=step;
-					if(n>=l) iLoop ? (n=0,iLoop--,funs.loop(iLoop)) : SPRITE.stop();
-				},piSpeed);
+					cur+=step;
+					//判断是否数字在范围外，如果是则判断是否循环
+					(reversed && cur<end || !reversed && cur>end) && (
+						loops ?
+						(cur=key,loops--,funs.loop(loops),console.log(123)) :
+						(SPRITE.stop(dir),funs[reversed ? 'rewound':'played']())
+					);
+				},options.speed);
 				return this;
+			},
+			forward:function(piSpeed,iLoop){
+				return this.play({
+					speed:piSpeed || 50,
+					loop:iLoop || 0,
+					from:0,
+					to:'all'
+				});
 			},
 			rewind:function(piSpeed,iLoop){
-				if(!piSpeed || isNaN(piSpeed)) piSpeed=50;
-				var imgs=THIS.find("img"),
-					l=imgs.length,
-					n=l-1,
-					SPRITE=this,
-					step=this.step;
-				n-=n%step;
-				timer=setInterval(function(){
-					imgs.eq(n).visible();
-					funs.show(n);
-					SPRITE.at=n;
-					var last=(n+step)%l;
-					imgs.eq(last).hidden();
-					funs.hide(last);
-					n-=step;
-					if(n<0) iLoop ? (n=l-1,iLoop--,funs.loop(iLoop)) : SPRITE.stop();
-				},piSpeed);
-				return this;
+				return this.play({
+					speed:piSpeed || 50,
+					loop:iLoop || 0,
+					from:'all',
+					to:0
+				});
 			},
-			stop:function(){
+			stop:function(dir){
 				clearInterval(timer);
 				timer=0;
-				funs.stop();
+				funs.stopped(dir);
 				return this;
 			},
 			show:function(n){
